a16d1bd63f6050e16de3f1fb4989811c1eb86cc8
Ornella Meneghini <meneghini.ornella@gmail.com>
2025-04-21 22:10:46 -0300

    Refactor U2 A5 and A6 to implement MySQL database interactions and display data in a table format

--- u2/u2-a5.php
+++ u2/u2-a5.php
@@ -8,8 +8,24 @@
 <body>
     <h2>Tabla</h2>
     <?php
-<?php
     // Modifique el programa desarrollado en la Actividad 4 para almacenar los datos del array en una tabla en una base de datos MySQL.
+
+    function connectDB() {
+        $connection = new mysqli("localhost", "root", "", "u2-a5");
+        if ($connection->connect_error) {
+            die("Error de Conexión: ".$connection->connect_error);
+        }
+        return $connection;
+    }
+
+    function createTable($connection) {
+        $connection->query("CREATE TABLE IF NOT EXISTS equipos (id INT AUTO_INCREMENT PRIMARY KEY, clave VARCHAR(30), valor INT)");
+    }
+
+    function deleteTable($connection) {
+        $connection->query("DELETE FROM equipos");
+    }
+
     function createTeams($size) {
         $equipos = [];
         for ($i = 1; $i <= $size; $i++) {
@@ -17,7 +33,15 @@
         }
         return $equipos;
     }
-    function createTable($equipos) {
+
+    function saveOnDB($connection, $equipos) {
+        $connection->query("DELETE FROM equipos");
+        foreach($equipos as $clave => $valor) {
+            $connection->query("INSERT INTO equipos (clave, valor) VALUES ('$clave', $valor)");
+        }
+    }
+
+    function showTable($equipos) {
         foreach($equipos as $clave => $valor) {
             echo ("
                 <tr>
@@ -27,6 +51,7 @@
             ");
         }
     }
+
     function sumValues($equipos) {
         $sum = 0;
         foreach($equipos as $valor) {
@@ -34,14 +59,19 @@
         }
         return $sum;
     }
+
+    $connection = connectDB();
+    createTable($connection);
     $equipos = createTeams(4);
+    saveOnDB($connection, $equipos);
+
     ?>
     <table>
         <tr>
             <th>Clave</th>
             <th>Valor</th>
         </tr>
-        <?php createTable($equipos); ?>
+        <?php showTable($equipos); ?>
     </table>
     <h2>Suma de valores</h2>
     <b><?php echo sumValues($equipos); ?></b>
--- u2/u2-a6.php
+++ u2/u2-a6.php
@@ -1,2 +1,75 @@
 <?php
-// Modifique el programa desarrollado en la Actividad 5 para enviar un correo electrónico con la suma de todos los elementos del vector.
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>U2 A4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+<body>
+    <h2>Tabla</h2>
+    <?php
+    // Modifique el programa desarrollado en la Actividad 5 para enviar un correo electrónico con la suma de todos los elementos del vector.
+
+    $conexion = new mysqli("localhost", "root", "", "u2-a5");
+    $conexion->query("
+        CREATE TABLE IF NOT EXISTS equipos (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            clave VARCHAR(30),
+            valor INT
+        )
+    ");
+
+    function createTeams($size) {
+        $equipos = [];
+        for ($i = 1; $i <= $size; $i++) {
+            $equipos["equipo$i"] = rand(1, 100);
+        }
+        return $equipos;
+    }
+    function insertIntoDB($conexion, $equipos) {
+        $conexion->query("DELETE FROM equipos");
+        $result = $conexion->prepare("
+            INSERT INTO equipos (clave, valor) VALUES (?, ?)
+        ");
+        foreach($equipos as $clave => $valor) {
+            $result = bind_params("yes", $clave, $valor);
+            $result->execute();
+        }
+        $result->close();
+    }
+    function createTable($equipos) {
+        foreach($equipos as $clave => $valor) {
+            echo ("
+                <tr class='row'>
+                    <th class='col'>$clave</th>
+                    <th class='col'>$valor</th>
+                </tr>
+            ");
+        }
+    }
+    function sumValues($equipos) {
+        $sum = 0;
+        foreach($equipos as $valor) {
+            $sum += $valor;
+        }
+        return $sum;
+    }
+    $equipos = createTeams(4);
+    ?>
+    <table class='container text-center'>
+        <tr class='row bg-info text-white'>
+            <th class='col'>Clave</th>
+            <th class='col'>Valor</th>
+        </tr>
+        <?php createTable($equipos); ?>
+    </table>
+    <h2>Suma de valores</h2>
+    <b class="display-1 text-success fw-bold"><?php echo sumValues($equipos); ?></b>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+</html>
